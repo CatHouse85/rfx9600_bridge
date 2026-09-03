bb1ed9f9811fc8435bbc6b606977e2c5a2a8f6f9
CatHouse85 <chris.guillem@libertysurf.fr>
2026-09-03 19:30:07 +0200

    Update Python command to use python3

--- rfx9600_bridge/run.sh
+++ rfx9600_bridge/run.sh
@@ -6,4 +6,4 @@ echo "Starting RFX9600 UDP → MQTT bridge..."
 # On pourrait plus tard lire un fichier de config, mais pour l’instant
 # les options seront passées via variables d’environnement par HA.
 
-python /app/rfx9600_listener.py
+python3 /app/rfx9600_listener.py
