7d2630539ded8a7050eb32295d7b418e24993635
CatHouse85 <chris.guillem@libertysurf.fr>
2026-09-04 19:25:58 +0200

    Add ls command to run.sh for debugging
    Added a command to list files in the /app directory before starting the bridge.

--- rfx9600_bridge/run.sh
+++ rfx9600_bridge/run.sh
@@ -5,7 +5,7 @@ echo "Starting RFX9600 UDP → MQTT bridge..."
 
 # On pourrait plus tard lire un fichier de config, mais pour l’instant
 # les options seront passées via variables d’environnement par HA.
-
+ls -l /app
 echo "Starting RFX9600 UDP → MQTT bridge2..."
 python - << 'EOF'
 print("Hello frm Python")
