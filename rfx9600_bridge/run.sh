f6e007e4d09761bb5795f9cc727429b75c0e0ba6
CatHouse85 <chris.guillem@libertysurf.fr>
2026-09-04 19:44:20 +0200

    Refactor run.sh for clarity and execution flow
    Removed commented code and updated echo statements for clarity.

--- rfx9600_bridge/run.sh
+++ rfx9600_bridge/run.sh
@@ -3,20 +3,9 @@ set -e
 
 echo "Starting RFX9600 UDP → MQTT bridge..."
 
-# On pourrait plus tard lire un fichier de config, mais pour l’instant
-# les options seront passées via variables d’environnement par HA.
 ls -l /app
-echo "Starting RFX9600 UDP → MQTT bridge2..."
-python - << 'EOF'
-print("Hello frm Python")
-# python /app/rfx9600_listener.py
 
-echo "Starting RFX9600 UDP → MQTT bridg3..."
+echo "Launching Python..."
+python /app/rfx9600_listener.py
 
-
-
-
-
-
-
-
+echo "Python finished."
