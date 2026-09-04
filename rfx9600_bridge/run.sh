6d4e9050fd268a223b01cc018fad386b47668e85
CatHouse85 <chris.guillem@libertysurf.fr>
2026-09-04 17:27:56 +0200

    Add additional startup messages to run.sh

--- rfx9600_bridge/run.sh
+++ rfx9600_bridge/run.sh
@@ -5,5 +5,6 @@ echo "Starting RFX9600 UDP → MQTT bridge..."
 
 # On pourrait plus tard lire un fichier de config, mais pour l’instant
 # les options seront passées via variables d’environnement par HA.
-
+echo "Starting RFX9600 UDP → MQTT bridge2..."
 python3 /app/rfx9600_listener.py
+echo "Starting RFX9600 UDP → MQTT bridge3..."
