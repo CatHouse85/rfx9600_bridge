28af2e5cd25e39b85311db0ad1e5693419b97ee2
CatHouse85 <chris.guillem@libertysurf.fr>
2026-09-04 13:11:36 +0200

    Delete rfx9600_bridge/run.sh

--- rfx9600_bridge/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-echo "Starting RFX9600 UDP → MQTT bridge..."
-
-# On pourrait plus tard lire un fichier de config, mais pour l’instant
-# les options seront passées via variables d’environnement par HA.
-
-python3 /app/rfx9600_listener.py
