1a321bc519fe8720ae4cbd743dd70de4173ac290
CatHouse85 <chris.guillem@libertysurf.fr>
2026-09-04 17:39:43 +0200

    Add files via upload

---
+++ rfx9600_bridge/run.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -e
+
+echo "Starting RFX9600 UDP → MQTT bridge..."
+
+# On pourrait plus tard lire un fichier de config, mais pour l’instant
+# les options seront passées via variables d’environnement par HA.
+
+echo "Starting RFX9600 UDP → MQTT bridge2..."
+
+python3 /app/rfx9600_listener.py
+
+echo "Starting RFX9600 UDP → MQTT bridg3..."
+
+
+
+
+
+
+
+
